08e180ced760862e28198091dd938050f42ad41e
IT21235092 <99312102+IT21235092@users.noreply.github.com>
2022-02-09 06:10:01 +0000

    completed 1

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,20 @@
 #include <stdio.h>
 
 int main() {
+
+  int mrk1,mrk2,tot=0;
+  float avg=0;
+
+  printf("\nEnter the marks of Subject 01 : ");
+  scanf("%d",&mrk1);
+
+  printf("\nEnter the marks of Subject 02 : ");
+  scanf("%d",&mrk2);
+
+  tot=mrk1+mrk2;
+  avg=tot/2.0;
+
+  printf("\nAverage : %.2f",avg);
   
   return 0;
 }
